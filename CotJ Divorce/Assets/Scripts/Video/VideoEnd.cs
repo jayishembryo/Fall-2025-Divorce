8998bab1537d922e922b68216da2f1ac9194466d
SkyBeal <thesnugpug1@gmail.com>
2025-09-20 17:12:57 -0500

    thank you

--- CotJ Divorce/Assets/Scripts/Video/VideoEnd.cs
+++ CotJ Divorce/Assets/Scripts/Video/VideoEnd.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class VideoEnd : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static bool VideoIsOver = false;
+    public static UnityEvent onVideoEnd = new UnityEvent();
+    private VideoPlayer vp;
+    public string NextScene;
+    private void Awake()
     {
-        
+        vp = GetComponent<VideoPlayer>();
+        vp.loopPointReached += VideoDone;
+        vp.loopPointReached += ctx => { onVideoEnd.Invoke(); };
     }
 
-    // Update is called once per frame
-    void Update()
+    private void VideoDone(VideoPlayer vp)
     {
-        
+        VideoIsOver = true;
+        SceneManager.LoadScene(NextScene);
     }
 }
