ef5a760a7ac925a00f595c92d09bfadf0086e2be
jayishembryo <kembry@mail.bradley.edu>
2025-09-20 13:42:22 -0500

    baby i dont care if fyoure a gooner

--- CotJ Divorce/Assets/Scripts/Bird/BirdController.cs
+++ CotJ Divorce/Assets/Scripts/Bird/BirdController.cs
@@ -1,16 +1,40 @@
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
 
 public class BirdController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    PlayerInput birdInput;
+    InputAction hitWindow;
+
+    [SerializeField] List<AudioClip> birdSounds = new List<AudioClip>();
+
+    [SerializeField] AudioSource glassSource;
+    [SerializeField] AudioSource playerSource;
+
+    public void Start()
     {
-        
+
+        birdInput = GetComponent<PlayerInput>();
+        hitWindow = birdInput.currentActionMap.FindAction("HitWindow");
+
+        hitWindow.started += HitWindow_started;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HitWindow_started(InputAction.CallbackContext context)
     {
-        
+
+        glassSource.Play();
+
+        int randomClip = Random.Range(0, birdSounds.Count);
+        playerSource.clip = birdSounds[randomClip];
+        playerSource.Play();
+
     }
+
 }
