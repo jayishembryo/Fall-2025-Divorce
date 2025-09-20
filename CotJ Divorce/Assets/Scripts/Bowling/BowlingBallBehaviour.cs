8de14d0f772b2d38eb4ec8594c319a5ba215e2ce
JacquelineHeisel3641 <117841731+JacquelineHeisel3641@users.noreply.github.com>
2025-09-19 22:38:57 -0500

    Ball go forward

--- CotJ Divorce/Assets/Scripts/Bowling/BowlingBallBehaviour.cs
+++ CotJ Divorce/Assets/Scripts/Bowling/BowlingBallBehaviour.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class BowlingBallBehaviour : MonoBehaviour
 {
+
+    public Rigidbody rb;
+    public float speed = 1;
+
+    public GameObject BowlingBall;
+
+    public bool IsBallThrown;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        IsBallThrown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IsBallThrown = true;
+        }
+
+        Bowl();
+    }
+
+    public void Bowl()
+    {
+        if (IsBallThrown)
+        {
+            //bowling ball go forward -> apply the velocity instead of doing it 1 update at a time, forcemode.impulse 
+            rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+            Debug.Log("Throw the ball");
+            IsBallThrown = false;
+        }
+        else
+        {
+            Debug.Log("GUTTTTTER");
+        }
     }
 }
