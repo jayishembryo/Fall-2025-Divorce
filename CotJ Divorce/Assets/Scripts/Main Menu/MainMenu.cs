e126f421322fc3c7513a56e597ba8d0c4a798e0d
jayishembryo <kembry@mail.bradley.edu>
2025-09-20 21:23:00 -0500

    ga

--- CotJ Divorce/Assets/Scripts/Main Menu/MainMenu.cs
+++ CotJ Divorce/Assets/Scripts/Main Menu/MainMenu.cs
@@ -6,6 +6,9 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] GameObject fakeCredits;
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip quitClip;
     [SerializeField] List<AudioClip> playTheGameSounds = new List<AudioClip>();
 
     public void StartGame()
@@ -33,7 +36,16 @@ public class MainMenu : MonoBehaviour
     {
 
         int randomClip = Random.Range(0, playTheGameSounds.Count);
+        audioSource.clip = playTheGameSounds[randomClip];
+        audioSource.Play();
 
+    }
+
+    public void OnQuitButtonHover()
+    {
+
+        audioSource.clip = quitClip;
+        audioSource.Play();
 
     }
 
